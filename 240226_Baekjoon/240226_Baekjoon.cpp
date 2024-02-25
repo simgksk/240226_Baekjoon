a8a0b3f35dd1cd1f23cca9cc7683bdaa581fc878
하나 <123716081+simgksk@users.noreply.github.com>
2024-02-26 01:07:11 +0900

    7595. Triangles

--- 240226_Baekjoon/240226_Baekjoon.cpp
+++ 240226_Baekjoon/240226_Baekjoon.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 	int main()
 	{
+#pragma region 7595. Triangles
 
+		int n;
+
+		while (true)
+		{
+			cin >> n;
+
+			if (n == 0)
+				break;
+
+			for (int i = 1; i <= n; i++)
+			{
+				for (int j = 1; j <= i; j++)
+					printf("*");
+				printf("\n");
+			}
+		}
+
+#pragma endregion
+		
+		return 0;
 	}
